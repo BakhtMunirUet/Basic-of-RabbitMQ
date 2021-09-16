47cc148a19cc198b8c514def56c41d5b3db307d7
BakhtMunirUet <bakhtmuniruet@gmail.com>
2021-09-16 11:45:43 -0700

    RabbitMQ basic

---
+++ RabbitMQConsumer/RabbitMQConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                //Uri = new Uri("amqp://guest:guest@localhost:8080")
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                //Port = 15672
+                
+            };
+
+            
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            FanoutExchangePublisher.Consume(channel);
+
+
+
+        }
+    }
+}
